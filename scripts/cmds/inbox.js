55199af4a54068861ccde510db54ae265e1e0794
tishabot5-star <tishabot5@gmail.com>
2026-03-01 19:56:03 +0600

    Update inbox.js

--- scripts/cmds/inbox.js
+++ scripts/cmds/inbox.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "inbox",
     aliases: ["in"],
     version: "1.7",
-    author: "MahMUD",
+    author: "𝐄𝐯𝐚𝐧 𝐕𝐚𝐢",
     countDown: 5,
     role: 0,
     category: "system"
@@ -16,8 +16,8 @@ module.exports = {
     }
 
       const query = encodeURIComponent(args.join(' '));
-      message.reply("𝐛𝐚𝐛𝐲 𝐜𝐡𝐞𝐜𝐤 𝐲𝐨𝐮𝐫 𝐢𝐧𝐛𝐨𝐱 🐤", event.threadID);
-      api.sendMessage("𝐡𝐢 𝐛𝐚𝐛𝐲😘", event.senderID);
+      message.reply("𝐍𝐚 𝐄𝐯𝐚𝐧 𝐂𝐡𝐚𝐮𝐝𝐡𝐚𝐫𝐲 𝐌𝐚𝐫𝐛𝐞 𝐀𝐦𝐤𝐞🥺 🐤", event.threadID);
+      api.sendMessage("𝐄𝐯𝐚𝐧 𝐂𝐡𝐚𝐮𝐝𝐡𝐚𝐫𝐲 𝐤 𝐧𝐨𝐤 𝐝𝐚😾😘", event.senderID);
     } catch (error) {
       console.error("error baby: " + error);
     }
